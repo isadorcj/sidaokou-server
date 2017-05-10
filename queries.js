b49cdac3f3162154de4b7769e07cbeb598dd9f3e
Chong <chong.jin@zafinlabs.com>
2017-05-10 01:12:35 -0400

    add project files

---
+++ queries.js
@@ -0,0 +1,20 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://192.168.2.28:5432/puppies';
+var db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+  getAllStoreNames: getAllStoreNames,
+  getSingleStore: getSingleStore,
+  createItem: createItem,
+  updateItem: updateItem,
+  removeItem: removeItem
+};
